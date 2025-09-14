e3a3b0186edea3b9186dfdf9c9a822fe1d15c086
guhongyang <guhongyang@sjtu.edu.cn>
2025-09-13 22:12:09 -0900

    bitstream update : split stream config

--- bitstream/config/stream.py
+++ bitstream/config/stream.py
@@ -2,7 +2,7 @@ from bitstream.config.base import BaseConfigModule
 from typing import List
 from bitstream.bit import Bit
 
-class MemoryAGConfig(BaseConfigModule):
+class MemoryAGConfig0(BaseConfigModule):
     FIELD_MAP = [
         ("mode", 1),
         ("base_addr", 29),
@@ -14,6 +14,16 @@ class MemoryAGConfig(BaseConfigModule):
         ("idx_tailing_low", 36),
         ("idx_tailing_up", 36),
         ("address_remapping", 64),
+    ]
+    
+class StreamCtrlConfig0(BaseConfigModule):
+    FIELD_MAP = [
+        ("ping_pong", 1),
+        ("pingpong_last_index", 3),
+    ]
+    
+class MemoryAGConfig1(BaseConfigModule):
+    FIELD_MAP = [
         ("idx", 15),
         ("idx_enable", 3),
         ("idx_keep_mode", 3),
@@ -28,15 +38,13 @@ class BufferAGConfig(BaseConfigModule):
         ("idx_enable", 2),
         ("idx_keep_mode", 2),
         ("idx_keep_last_index", 6),
-        ("ping_buffer", 3),
-        ("pong_buffer", 3),
     ]
 
 
-class StreamCtrlConfig(BaseConfigModule):
+class StreamCtrlConfig1(BaseConfigModule):
     FIELD_MAP = [
-        ("ping_pong", 1),
-        ("pingpong_last_index", 3),
+        ("ping_buffer", 3),
+        ("pong_buffer", 3),
     ]
 
 
@@ -46,36 +54,26 @@ class StreamConfig(BaseConfigModule):
     def __init__(self, idx: int):
         super().__init__()
         self.idx = idx
-        self.memory_ag = MemoryAGConfig()
-        self.buffer_ag = BufferAGConfig()
-        self.ctrl = StreamCtrlConfig()
+        # submodules: (json_key, module_instance)
+        self.submodules = [
+            ("memory_AG", MemoryAGConfig0()),
+            ("stream", StreamCtrlConfig0()),
+            ("memory_AG", MemoryAGConfig1()),
+            ("buffer_AG", BufferAGConfig()),
+            ("stream", StreamCtrlConfig1()),
+        ]
 
     def from_json(self, cfg: dict):
-        """
-        Expected JSON structure (order may vary):
-        {
-            "stream0": {
-                "memory_AG": {...},
-                "buffer_AG": {...},
-                "stream": {...}
-            },
-            "stream1": {...}
-        }
-        """
-        
         cfg = cfg.get("stream_engine", cfg)
         key = f"stream{self.idx}"
         if key not in cfg:
             return
         stream_cfg = cfg[key]
 
-        if "memory_AG" in stream_cfg:
-            self.memory_ag.from_json(stream_cfg["memory_AG"])
-        if "buffer_AG" in stream_cfg:
-            self.buffer_ag.from_json(stream_cfg["buffer_AG"])
-        if "stream" in stream_cfg:
-            self.ctrl.from_json(stream_cfg["stream"])
+        for json_key, sub in self.submodules:
+            if json_key in stream_cfg:
+                sub.from_json(stream_cfg[json_key])
 
     def to_bits(self) -> List[Bit]:
-        """Concatenate all sub-config bitstreams."""
-        return self.memory_ag.to_bits() + self.buffer_ag.to_bits() + self.ctrl.to_bits()
+        """Concatenate all sub-config bitstreams in fixed order."""
+        return sum((sub.to_bits() for _, sub in self.submodules), [])
